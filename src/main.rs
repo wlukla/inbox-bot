645b15d633a0497c2750ec481bcf2132063abc08
wlukla <vladislav.danilochkin@gmail.com>
2021-10-12 23:34:52 +0300

    feat: add pong response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use std::env;
+
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    teloxide::enable_logging!();
+    log::info!("Starting inbox bot...");
+
+    let bot = Bot::from_env().auto_send();
+
+    teloxide::repl(bot, |message| async move {
+        message.answer("pong!").await?;
+        respond(())
+    })
+    .await;
 }
