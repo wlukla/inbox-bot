548bc6844f4ea9756ed685a13be73b92b4a12bf5
wlukla <vladislav.danilochkin@gmail.com>
2021-10-17 01:15:37 +0300

    feat: add message saving to notion

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,51 @@
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
+
 use dotenv::dotenv;
 use std::env;
 
-use hyper::{body::HttpBody as _, Body, Client, Method, Request};
+use hyper::{Body, Client, Method, Request};
 use hyper_tls::HttpsConnector;
 use teloxide::prelude::*;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() {
     dotenv().ok();
 
-    let notion_base_url = env::var("NOTION_BASE_URL").unwrap();
-    let notion_token = env::var("NOTION_TOKEN").unwrap();
-    let notion_database_id = env::var("NOTION_DB_ID").unwrap();
+    teloxide::enable_logging!();
+    log::info!("Starting inbox bot...");
 
-    println!("url: {}, token: {}", notion_base_url, notion_token);
-    let url = (format!("{}/v1/databases/{}", notion_base_url, notion_database_id))
-        .parse::<hyper::Uri>()
-        .unwrap();
+    let bot = Bot::from_env().auto_send();
 
-    make_request(url).await
+    teloxide::repl(bot, |message| async move {
+        match message.update.text() {
+            Some(text) => {
+                make_request(String::from(text)).await;
+                message.answer("Saved!").await?;
 
-    // teloxide::enable_logging!();
-    // log::info!("Starting inbox bot...");
+                respond(())
+            }
+            None => {
+                message.answer("Send text message please").await?;
 
-    // let bot = Bot::from_env().auto_send();
-
-    // teloxide::repl(bot, |message| async move {
-    //     message.answer("pong!").await?;
-    //     respond(())
-    // })
-    // .await;
+                respond(())
+            }
+        }
+    })
+    .await;
 }
 
-async fn make_request(url: hyper::Uri) -> Result<()> {
+async fn make_request(content: String) -> Result<()> {
+    let notion_base_url = env::var("NOTION_BASE_URL").unwrap();
+    let notion_page_id = env::var("NOTION_PAGE_ID").unwrap();
+    let url = (format!("{}/v1/blocks/{}/children", notion_base_url, notion_page_id))
+        .parse::<hyper::Uri>()
+        .unwrap();
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
 
@@ -42,25 +53,63 @@ async fn make_request(url: hyper::Uri) -> Result<()> {
     let notion_token = env::var("NOTION_TOKEN").unwrap();
     auth_header.push_str(notion_token.as_str());
 
+    let new_block = Block::new(String::from(content));
+    let req_body = serde_json::to_string_pretty(&new_block).unwrap();
+
     let request = Request::builder()
-        .method(Method::GET)
+        .method(Method::PATCH)
         .uri(url)
         .header("Authorization", auth_header)
         .header("Notion-Version", "2021-08-16")
-        .body(Body::from(""))?;
+        .header("Content-Type", "application/json")
+        .body(Body::from(req_body))?;
 
-    let mut res = client.request(request).await?;
-
-    println!("Repsonse: {}", res.status());
-    println!("Headers: {:#?}", res.headers());
-
-    while let Some(next) = res.data().await {
-        let chunk = next?;
-        let text = &chunk;
-        println!("{:?}", text);
-    }
-
-    println!("\n\nDone!");
+    client.request(request).await?;
 
     Ok(())
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Text {
+    content: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ParagraphText {
+    r#type: String,
+    text: Text,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Paragraph {
+    text: [ParagraphText; 1],
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Children {
+    object: String,
+    r#type: String,
+    paragraph: Paragraph,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Block {
+    children: [Children; 1],
+}
+
+impl Block {
+    fn new(content: String) -> Block {
+        Block {
+            children: [Children {
+                object: String::from("block"),
+                r#type: String::from("paragraph"),
+                paragraph: Paragraph {
+                    text: [ParagraphText {
+                        r#type: String::from("text"),
+                        text: Text { content },
+                    }],
+                },
+            }],
+        }
+    }
 }
